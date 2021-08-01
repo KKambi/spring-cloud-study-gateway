2fcc4b03f589f0e23f1d29b9f922bb979160f71b
KKambi <bok03220@gmail.com>
2021-08-02 00:21:09 +0900

    refactor: yml 버전 request / response header 연동

--- src/main/java/com/example/springcloudstudygateway/config/FilterConfig.java
+++ src/main/java/com/example/springcloudstudygateway/config/FilterConfig.java
@@ -8,19 +8,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
 
-    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/order-service/**")
-                        .filters(f -> f.addRequestHeader("order-request", "order-request-header")
-                                .addResponseHeader("order-response", "order-response-header"))
-                        .uri("http://localhost:8081/")
-                )
-                .route(r -> r.path("/user-service/**")
-                        .filters(f -> f.addRequestHeader("user-request", "user-request-header")
-                                .addResponseHeader("user-response", "user-response-header"))
-                        .uri("http://localhost:8082/")
-                )
-                .build();
-    }
+//    @Bean
+//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(r -> r.path("/order-service/**")
+//                        .filters(f -> f.addRequestHeader("order-request", "order-request-header")
+//                                .addResponseHeader("order-response", "order-response-header"))
+//                        .uri("http://localhost:8081/")
+//                )
+//                .route(r -> r.path("/user-service/**")
+//                        .filters(f -> f.addRequestHeader("user-request", "user-request-header")
+//                                .addResponseHeader("user-response", "user-response-header"))
+//                        .uri("http://localhost:8082/")
+//                )
+//                .build();
+//    }
 }
