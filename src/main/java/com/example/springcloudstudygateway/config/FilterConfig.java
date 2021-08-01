0bdaed7ebfdb3d2aa6458c2f9a8c7bdde6b1b5ea
KKambi <bok03220@gmail.com>
2021-08-02 00:09:15 +0900

    init

---
+++ src/main/java/com/example/springcloudstudygateway/config/FilterConfig.java
@@ -0,0 +1,26 @@
+package com.example.springcloudstudygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/order-service/**")
+                        .filters(f -> f.addRequestHeader("order-request", "order-request-header")
+                                .addResponseHeader("order-response", "order-response-header"))
+                        .uri("http://localhost:8081/")
+                )
+                .route(r -> r.path("/user-service/**")
+                        .filters(f -> f.addRequestHeader("user-request", "user-request-header")
+                                .addResponseHeader("user-response", "user-response-header"))
+                        .uri("http://localhost:8082/")
+                )
+                .build();
+    }
+}
